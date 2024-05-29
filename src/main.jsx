6b93e7f109f48981fbaf459607947d342c92b09a
HyuDeQueue <quanghuy01062004@gmail.com>
2024-05-30 02:54:47 +0700

    Adding clientID

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import AppWrapper from './App';
 
-const CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID_HERE';
+const CLIENT_ID = '200707558272-te7t2gl0frolmfep3rolgc1l8k71drgb.apps.googleusercontent.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
