47ae008c82e2b0f20276f391f0c7004ce9969611
NguyenHipHop <153606104+NguyenHipHop@users.noreply.github.com>
2024-05-29 19:50:07 +0700

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { RouterProvider } from "react-router-dom";
-
-import React from "react";
-import { route } from "routes";
-import { useSelector } from "react-redux";
+import HeaderAs from "./HeaderAs";
+import SideBar from "./SideBar";
 
 function App() {
-  // @ts-ignore
-  const user = useSelector((state) => state.user);
-  console.log(user);
-
-  return <RouterProvider router={route} />;
+  return (
+    <>
+      <HeaderAs />
+      <SideBar />
+    </>
+  );
 }
 
 export default App;
