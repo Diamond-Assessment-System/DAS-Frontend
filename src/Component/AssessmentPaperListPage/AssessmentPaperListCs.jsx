5e50f1c081247e1d172ecf1b95732bd3f9cf108d
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-07-03 16:08:46 +0700

    fix missing page

--- src/Component/AssessmentPaperListPage/AssessmentPaperListCs.jsx
+++ src/Component/AssessmentPaperListPage/AssessmentPaperListCs.jsx
@@ -43,9 +43,9 @@ function AssessmentPaperListCs() {
     );
   }
 
-  if (error) {
-    return <div className="error-message">{error}</div>;
-  }
+  // if (error) {
+  //   return <div className="error-message">{error}</div>;
+  // }
 
 
   return (
