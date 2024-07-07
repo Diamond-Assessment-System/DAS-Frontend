e96606f3217c217fcec9f5213f9fd29675e29095
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2024-07-07 13:24:26 +0700

    Update ContactModal to HomePage

--- src/Component/HomePage/HomePage.jsx
+++ src/Component/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import sampleImage from "../../assets/backgrounddas.png"; // Đảm bảo đường dẫn đến ảnh là chính xác
+import ContactModal from "../contact/ContactModal"; // Đảm bảo đường dẫn đến ContactModal là chính xác
 
 function MyComponent() {
   return (
     <>
-      <div className="flex justify-center ">
+      <div className="flex justify-center">
         <img src={sampleImage} alt="Sample" className="h-full w-full" />
+        <ContactModal />
       </div>
     </>
   );
