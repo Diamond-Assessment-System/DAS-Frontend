fbf37b3cd5af0f30fe41b54b65a559b5d2108a36
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2024-07-08 11:01:36 +0700

    Upload css DiamondInformation

--- src/Component/DiamondInformationPage/DiamondInformation.jsx
+++ src/Component/DiamondInformationPage/DiamondInformation.jsx
@@ -4,6 +4,7 @@ import SanHo from '../../assets/San-ho.jpg';
 import Zircon from '../../assets/Zircon.jpg';
 import Tanzanite from '../../assets/Tanzanite.jpg';
 import { Link } from 'react-router-dom';
+import './DiamondInformation.css'; // Import CSS file for styling
 
 function DiamondInformation() {
   const [hoveredImage, setHoveredImage] = React.useState(null);
@@ -36,7 +37,7 @@ function DiamondInformation() {
               </Link>
             </div>
             <div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left">
-              <h2 className="text-3xl font-bold text-black">{item.name}</h2>
+              <h2 className={`text-3xl font-bold ${hoveredImage === item.image ? "hovered" : ""}`}>{item.name}</h2>
               <p className="text-lg text-yellow-600 mt-4">Mô Tả</p>
               <Link to={`/diamonds/${item.name.toLowerCase()}`}>
                 <button className="mt-8 py-2 px-4 bg-amber-300 text-black font-semibold rounded-md shadow hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-300 focus:ring-offset-2">
