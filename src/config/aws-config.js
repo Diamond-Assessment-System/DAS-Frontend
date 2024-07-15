c0bab8e429264fdf1b2706ed36c357f689f3707f
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-07-15 23:34:24 +0700

    done aws

--- src/config/aws-config.js
+++ src/config/aws-config.js
@@ -18,10 +18,6 @@
 
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-console.log('AWS Access Key ID:', import.meta.env.VITE_AWS_ACCESS_KEY_ID);  // Kiểm tra giá trị của biến môi trường
-console.log('AWS Secret Access Key:', import.meta.env.VITE_AWS_SECRET_ACCESS_KEY);  // Kiểm tra giá trị của biến môi trường
-console.log('AWS Region:', import.meta.env.VITE_AWS_REGION);  // Kiểm tra giá trị của biến môi trường
-
 const s3Client = new S3Client({
   region: 'ap-southeast-2',
   credentials: {
