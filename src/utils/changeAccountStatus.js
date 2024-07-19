1875e0b8944bc9184d59d8d3d1c6434755357daf
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-07-19 17:54:51 +0700

    Block users function

--- src/utils/changeAccountStatus.js
+++ src/utils/changeAccountStatus.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'https://das-backend.fly.dev/api/accounts';
 
-export const changeAccountStatus = async (accountId, status) => {
+export const changeAccountStatus = async (accountId, status, reason) => {
   try {
-    const response = await axios.put(`${API_URL}/${accountId}/status/${status}`);
+    const response = await axios.put(`${API_URL}/${accountId}/status/${status}`, reason, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
     return response.data;
   } catch (error) {
     console.error('Error changing user status:', error);
