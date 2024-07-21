72a689155336fc725958fb2a6a8472e3b4ce181e
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-07-21 21:38:57 +0700

    Phan quyen

--- src/utils/checkRole.js
+++ src/utils/checkRole.js
@@ -6,8 +6,8 @@ export const checkRole = async (accountId) => {
     try {
         const response = await axios.get(`${API_URL}/${accountId}`);
         const accountData = response.data;
-        //return accountData.role;
-        return 6;
+        return accountData.role;
+        //return 6;
     } catch (error) {
         console.error('Error fetching account data:', error);
         throw error;
