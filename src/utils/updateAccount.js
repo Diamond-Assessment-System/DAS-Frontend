1f031ab6e7bd0831dfe122f7c7d8994ba2ec508f
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-07-26 15:22:29 +0700

    update profile

---
+++ src/utils/updateAccount.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'https://das-backend.fly.dev/api/accounts';
+
+export const updateProfile = async (accountId, info) => {
+  try {
+    const response = await axios.put(`${API_URL}/${accountId}`, info);
+    return response.data;
+  } catch (error) {
+    console.error('Error changing user status:', error);
+    throw error;
+  }
+};
