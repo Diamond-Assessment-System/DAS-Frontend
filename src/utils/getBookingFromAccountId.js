41b7d7f487c69198f3cffc7440fda1ed3226d7d3
Hyudeque <quanghuy01062004@gmail.com>
2024-08-01 15:13:04 +0700

    Create getBookingFromAccountId.js

---
+++ src/utils/getBookingFromAccountId.js
@@ -0,0 +1,22 @@
+// getBookingFromId.js
+
+import axios from 'axios';
+
+async function getBookingFromAccountId(accountId) {
+    try {
+        const response = await axios.get(`https://das-backend.fly.dev/api/assessment-bookings/account/${accountId}`);
+        return response.data; // Return the fetched service data
+    } catch (error) {
+        if (error.response) {
+            console.error('Server responded with a status:', error.response.status);
+            console.error('Response data:', error.response.data);
+        } else if (error.request) {
+            console.error('Request made but no response received:', error.request);
+        } else {
+            console.error('Error setting up the request:', error.message);
+        }
+        throw error; // Rethrow the error to handle it in the component
+    }
+}
+
+export default getBookingFromAccountId;
